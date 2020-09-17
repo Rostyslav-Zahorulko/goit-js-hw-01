1ce0cbd8e9941399d5146206738b2eeec6f94f6e
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-09-17 09:06:16 +0300

    Додає файли в ДЗ

---
+++ js/task-3.js
@@ -0,0 +1,25 @@
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message = prompt('Введите пароль');
+
+// if (message === null) {
+//   message = 'Отменено пользователем!';
+// } else if (message === ADMIN_PASSWORD) {
+//   message = 'Добро пожаловать!';
+// } else {
+//   message = 'Доступ запрещен, неверный пароль!';
+// }
+
+// alert(message);
+
+switch (message) {
+  case null:
+    message = 'Отменено пользователем!';
+    break;
+  case ADMIN_PASSWORD:
+    message = 'Добро пожаловать!';
+    break;
+  default:
+    message = 'Доступ запрещен, неверный пароль!';
+}
+
+alert(message);
---
+++ js/task-6.js
@@ -0,0 +1,88 @@
+// let input = prompt('Введите число');
+// let total = 0;
+
+// while (input !== null) {
+//   input;
+//   // input = Number(input);
+//   console.log('Введенное число: ', input);
+
+//   if (input === NaN) {
+//     alert('Было введено не число, попробуйте еще раз');
+//     continue;
+//   }
+
+//   total += input;
+//   console.log('Общая сумма: ', total);
+// }
+
+// alert(`Общая сумма чисел равна ${total}`);
+
+// Напівробочий варіант
+
+// let input;
+// let total = 0;
+
+// do {
+//   input = prompt('Введите число');
+//   input = Number(input);
+//   console.log('Введенное число: ', input);
+//   if (input === NaN) {
+//     alert('Было введено не число, попробуйте еще раз');
+//     continue;
+//   }
+//   total += input;
+//   console.log('Общая сумма: ', total);
+// } while (input !== 4);
+
+// alert(`Общая сумма чисел равна ${total}`);
+
+// while (input !== null) {
+//   input = prompt('Введите число');
+//   input = Number(input);
+//   total += input;
+//   console.log('fff');
+// }
+
+// for (input; input !== null; input += 1) {
+//   total += input;
+// }
+
+// while (input !== null) {
+//   total += input;
+//   console.log(total);
+// }
+
+// do {
+//   total += input;
+//   console.log(total);
+// } while (input !== null);
+
+// let input;
+// let total = 0;
+
+// for (let i = 0; i < 5; i += 1) {
+//   input = prompt('Введите число');
+//   input = Number(input);
+//   console.log('Введенное число: ', input);
+//   total += input;
+//   console.log('Общая сумма: ', total);
+// }
+
+// alert(`Общая сумма чисел равна ${total}`);
+
+let input;
+let total = 0;
+
+while (input !== 0) {
+  input = prompt('Введите число');
+  input = Number(input);
+  console.log('Введенное число: ', input);
+  //   if (input === NaN) {
+  //     alert('Было введено не число, попробуйте еще раз');
+  //     continue;
+  //   }
+  total += input;
+  console.log('Общая сумма: ', total);
+}
+
+alert(`Общая сумма чисел равна ${total}`);
